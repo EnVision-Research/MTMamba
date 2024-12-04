97664eb7888b2ccc5ba2c739b43a12f9f073d4c3
Baijiong Lin <bj.lin.email@gmail.com>
2024-12-04 20:35:56 +0800

    Update run.sh

--- pretrained_ckpts/run.sh
+++ pretrained_ckpts/run.sh
@@ -1,2 +1,2 @@
 wget https://github.com/SwinTransformer/storage/releases/download/v1.0.0/swin_large_patch4_window12_384_22kto1k.pth
-python swin2mmseg.py --src ./swin_large_patch4_window12_384_22kto1k.pth
+python swin2mmseg.py swin_large_patch4_window12_384_22kto1k.pth
